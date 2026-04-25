bd65d9265bfaeffd9124f7fa2238fada8da56b64
Ashutosh Upreti <ashutoshishunishu@gmail.com>
2026-04-25 22:04:31 +0530

    Fix formatting in LoginServlet.java

--- src/main/java/com/examprepplanner/servlet/LoginServlet.java
+++ src/main/java/com/examprepplanner/servlet/LoginServlet.java
@@ -15,7 +15,6 @@ public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
